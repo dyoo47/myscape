d8d526cf3522e37d8f958fac1ad2d3ae105f334c
dyoo47 <34610019+dyoo47@users.noreply.github.com>
2023-08-12 16:11:59 -0700

    Adjust column width, add GitHub button

---
+++ src/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let href: string;
+</script>
+
+<a
+	target="_blank"
+	class="h2 mt-auto h-min opacity-60 transition duration-300 ease-in-out hover:opacity-100"
+	{href}
+>
+	<slot />
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import type { LinkedInDataEntry } from '$lib/constants';
+	import { faGithub } from '@fortawesome/free-brands-svg-icons';
+	import Fa from 'svelte-fa';
+	import Button from '../components/Button.svelte';
 	import JobCard from '../components/JobCard.svelte';
 	import Placeholder from '../components/Placeholder.svelte';
 	import Search from '../components/Search.svelte';
@@ -14,10 +17,16 @@
 	};
 </script>
 
-<div class="container px-4 mt-20 mx-auto flex justify-center">
-	<div class="space-y-5">
-		<h1 class="h1">myscape - job board webscraper</h1>
+<div style="max-width: 960px" class="px-4 mt-20 mx-auto flex justify-center">
+	<div class="space-y-5 w-full">
+		<div class="grid grid-flow-col">
+			<h1 class="h1">myscape - job board webscraper</h1>
+			<div class="flex justify-end">
+				<Button href="https://github.com/dyoo47/myscape"><Fa icon={faGithub} /></Button>
+			</div>
+		</div>
 		<Search {fetchData} />
+
 		<div class="row">
 			{#if data && !loading}
 				{#each data as entry}
