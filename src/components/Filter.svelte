e926ff3ccddd7795c3b4227778f48c2250ebc714
dyoo47 <34610019+dyoo47@users.noreply.github.com>
2023-08-15 20:53:05 -0700

    Make buttons toggleable

---
+++ src/components/Filter.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let selected: boolean = false;
+</script>
+
+<button
+	on:click={() => {
+		selected = !selected;
+	}}
+	type="button"
+	class={selected
+		? 'btn-sm rounded-lg transition-all ease-in-out duration-150 variant-filled-primary'
+		: 'btn-sm rounded-lg transition-all ease-in-out duration-150 variant-outline-primary'}
+>
+	<slot />
+</button>
--- src/components/FilterModal.svelte
+++ src/components/FilterModal.svelte
@@ -1,5 +1,21 @@
-<div class="bg-surface-500 rounded-lg">
-	<div class="flex space-x-2 p-5">
-		<span>filters:</span> <span class="chip variant-filled-primary">entry level</span>
+<script>
+	import Filter from './Filter.svelte';
+</script>
+
+<div class="bg-surface-500 py-5 rounded-lg">
+	<div class="flex space-x-2 px-5 align-middle items-center">
+		<span>seniority:</span>
+		<Filter>internship</Filter>
+		<Filter>entry level</Filter>
+		<Filter>mid level</Filter>
+		<Filter>senior level</Filter>
+	</div>
+	<hr class="my-2 h-0.5 border-0 bg-surface-600" />
+	<div class="flex space-x-2 px-5 align-middle items-center">
+		<span>pay range:</span>
+		<Filter>internship</Filter>
+		<Filter>entry level</Filter>
+		<Filter>mid level</Filter>
+		<Filter>senior level</Filter>
 	</div>
 </div>
